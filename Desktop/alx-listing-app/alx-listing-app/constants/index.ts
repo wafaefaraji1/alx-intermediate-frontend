813b48e54118b01bead5403f120e145b7cb74b7f
wafae faraji <farajiwafae93@gmail.com>
2025-11-04 12:31:31 +0100

    Delete Desktop directory

--- Desktop/alx-listing-app/alx-listing-app/constants/index.ts
+++
@@ -1,4 +0,0 @@
-export const API_URL = "https://example.com/api";
-export const APP_NAME = "ALX Listing App";
-export const API_URL = "https://example.com/api";
-export const APP_NAME = "ALX Listing App";
